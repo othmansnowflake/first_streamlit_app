aeb286378ec79a0cb193dff24baa3eae63371a3f
othmansnowflake <148855780+othmansnowflake@users.noreply.github.com>
2023-10-25 11:23:19 +0200

    Update streamlit_app.py
    added import requests section

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+#New Section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
-
 
