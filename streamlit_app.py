616a85b2530ac654feb882304717aa93d4770074
othmansnowflake <148855780+othmansnowflake@users.noreply.github.com>
2023-10-25 13:52:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #add add_fruit textbar
-add_fruit = streamlit.text_input('What fruit would you like to add'')
+add_fruit = streamlit.text_input('What fruit would you like to add')
 streamlit.write('Thanks for adding ', fruit_choice)
